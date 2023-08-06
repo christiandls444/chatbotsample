3ee388a2e6f818f4665db4490ad37fbdbe7cc825
Your GitHub Username <your_email@example.com>
2023-08-07 04:43:34 +0800

    adding openpyxl

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import openpyxl  # Add this line
 
 # Load the dataset
 dataset_path = "chatbot.xlsx"  # Update with the actual path
